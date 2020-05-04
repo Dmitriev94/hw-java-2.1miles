32f85dbf37e6d18cfb6cbeb49cf21cf373163cd5
Ivan Dmitriev <dmitrievstekloy@gmail.com>
2020-05-04 15:06:34 +0300

    project done

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double ticketcost = 35720.42; // вводим стоимость билетов
+        int bonusmiles = (int) (ticketcost / 20);
+        System.out.println(bonusmiles);
+
+    }
+}
+
+
